8f673919c5e72cb3e7781dd271e458bc648069c3
Jagat Jeevan Sahoo <jagatsah@thoughtworks.com>
2018-02-07 20:00:10 +0530

    test add to home screen

--- src/index.js
+++ src/index.js
@@ -4,14 +4,28 @@ import App from './components/App';
 new App().render();
 
 const init = () => {
-    if ('serviceWorker' in navigator) {
+  if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/dist/sw.js').then(registration => {
+      navigator.serviceWorker.register('/dist/sw.js').then(registration => {
         console.log('SW registered: ', registration);
-        }).catch(registrationError => {
+      }).catch(registrationError => {
         console.log('SW registration failed: ', registrationError);
-        });
+      });
     });
-    }
+
+    window.addEventListener('beforeinstallprompt', (e) => {
+      e.userChoice.then(function(choiceResult) {
+
+        console.log(choiceResult.outcome);
+
+        if(choiceResult.outcome == 'dismissed') {
+          console.log('User cancelled home screen install');
+        }
+        else {
+          console.log('User added to home screen');
+        }
+      });
+    });
+  }
 }
 init();
